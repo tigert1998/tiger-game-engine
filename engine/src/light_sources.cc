c98e675ee251a135cb6e8ed7525bb7b049bea2bc
Xiaohu Tang <tigertang.zju@qq.com>
2023-05-09 15:16:18 +0800

    Add support for PBR

--- engine/src/light_sources.cc
+++ engine/src/light_sources.cc
@@ -77,35 +77,35 @@ REG_LIGHT(Point, 8)
 
 #undef REG_LIGHT
 
-vec3 calcAmbient(vec3 ka) {
+vec3 CalcAmbient(vec3 ka) {
     return ka;
 }
 
-vec3 calcDiffuse(vec3 lightDirection, vec3 normal, vec3 kd) {
+vec3 CalcDiffuse(vec3 lightDirection, vec3 normal, vec3 kd) {
     float diffuse = max(dot(normalize(lightDirection), normalize(normal)), 0);
     return diffuse * kd;
 }
 
-vec3 calcSpecular(vec3 lightDirection, vec3 normal, vec3 viewDirection, float shininess, vec3 ks) {
+vec3 CalcSpecular(vec3 lightDirection, vec3 normal, vec3 viewDirection, float shininess, vec3 ks) {
     float specular = dot(reflect(normalize(-lightDirection), normalize(normal)), normalize(viewDirection));
     specular = max(specular, 0);
     specular = pow(specular, shininess);
     return specular * ks;
 }
 
-vec3 calcPhongLighting(
+vec3 CalcPhongLighting(
     vec3 ka, vec3 kd, vec3 ks,
     vec3 normal, vec3 cameraPosition, vec3 position,
     float shininess
 ) {
     vec3 color = vec3(0);
     for (int i = 0; i < uAmbientLightCount; i++) {
-        color += calcAmbient(ka) * uAmbientLights[i].color;
+        color += CalcAmbient(ka) * uAmbientLights[i].color;
     }
     for (int i = 0; i < uDirectionalLightCount; i++) {
-        color += calcDiffuse(-uDirectionalLights[i].dir, normal, kd) *
+        color += CalcDiffuse(-uDirectionalLights[i].dir, normal, kd) *
             uDirectionalLights[i].color;
-        color += calcSpecular(-uDirectionalLights[i].dir, normal, cameraPosition - position, shininess, ks) *
+        color += CalcSpecular(-uDirectionalLights[i].dir, normal, cameraPosition - position, shininess, ks) *
             uDirectionalLights[i].color;
     }
     for (int i = 0; i < uPointLightCount; i++) {
@@ -113,11 +113,110 @@ vec3 calcPhongLighting(
         float dis = distance(uPointLights[i].pos, position);
         vec3 pointLightColor = uPointLights[i].color /
             (attenuation.x + attenuation.y * dis + attenuation.z * pow(dis, 2));
-        color += calcDiffuse(uPointLights[i].pos - position, normal, kd) *
+        color += CalcDiffuse(uPointLights[i].pos - position, normal, kd) *
             pointLightColor;
-        color += calcSpecular(uPointLights[i].pos - position, normal, cameraPosition - position, shininess, ks) *
+        color += CalcSpecular(uPointLights[i].pos - position, normal, cameraPosition - position, shininess, ks) *
             pointLightColor;
     }
+    return color;
+}
+
+float DistributionGGX(vec3 normal, vec3 halfway, float roughness) {
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float nDotH = max(dot(normal, halfway), 0.0);
+    float nDotH2 = nDotH * nDotH;
+
+    float num = a2;
+    float denom = (nDotH2 * (a2 - 1.0) + 1.0);
+    const float PI = radians(180);
+    denom = PI * denom * denom;
+
+    return num / denom;
+}
+
+float GeometrySchlickGGX(vec3 normal, vec3 viewDirection, float roughness) {
+    float k = pow(roughness + 1.0, 2) / 8.0;
+
+    float nDotV = max(dot(normal, viewDirection), 0.0);
+    float num = nDotV;
+    float denom = nDotV * (1.0 - k) + k;
+
+    return num / denom;
+}
+
+float GeometrySmith(vec3 normal, vec3 viewDirection, vec3 lightDirection, float roughness) {
+    float ggx2  = GeometrySchlickGGX(normal, viewDirection, roughness);
+    float ggx1  = GeometrySchlickGGX(normal, lightDirection, roughness);
+
+    return ggx1 * ggx2;
+}
+
+vec3 FresnelSchlick(vec3 halfway, vec3 viewDirection, vec3 f0) {
+    float hDotV = max(dot(halfway, viewDirection), 0.0);
+    return f0 + (1.0 - f0) * pow(clamp(1.0 - hDotV, 0.0, 1.0), 5.0);
+}
+
+vec3 CalcPBRLightingForSingleLightSource(
+    vec3 albedo, float metallic, float roughness,
+    vec3 normal, vec3 viewDirection, vec3 lightDirection,
+    vec3 lightColor
+) {
+    normal = normalize(normal);
+    viewDirection = normalize(viewDirection);
+    lightDirection = normalize(lightDirection);
+    vec3 halfway = normalize(viewDirection + lightDirection);
+
+    vec3 f0 = mix(vec3(0.04), albedo, metallic);
+
+    // Cook-Torrance BRDF
+    float NDF = DistributionGGX(normal, halfway, roughness);
+    float G = GeometrySmith(normal, viewDirection, lightDirection, roughness); 
+    vec3 F = FresnelSchlick(halfway, viewDirection, f0);
+
+    vec3 kd = (vec3(1.0) - F) * (1.0 - metallic);
+
+    vec3 numerator = NDF * G * F;
+    float denominator = 4.0 * max(dot(normal, viewDirection), 0.0) * max(dot(normal, lightDirection), 0.0) + 1e-6;
+    vec3 specular = numerator / denominator;
+
+    const float PI = radians(180);
+    float nDotL = max(dot(normal, lightDirection), 0.0);
+    return (kd * albedo / PI + specular) * lightColor * nDotL;
+}
+
+vec3 CalcPBRLighting(
+    vec3 albedo, float metallic, float roughness, float ao,
+    vec3 normal, vec3 cameraPosition, vec3 position
+) {
+    vec3 color = vec3(0);
+
+    for (int i = 0; i < uAmbientLightCount; i++) {
+        color += albedo * uAmbientLights[i].color * ao;
+    }
+
+    for (int i = 0; i < uDirectionalLightCount; i++) {
+        color += CalcPBRLightingForSingleLightSource(
+            albedo, metallic, roughness,
+            normal, cameraPosition - position, -uDirectionalLights[i].dir,
+            uDirectionalLights[i].color
+        );
+    }
+    for (int i = 0; i < uPointLightCount; i++) {
+        vec3 attenuation = uPointLights[i].attenuation;
+        float dis = distance(uPointLights[i].pos, position);
+        vec3 pointLightColor = uPointLights[i].color /
+            (attenuation.x + attenuation.y * dis + attenuation.z * pow(dis, 2));
+        color += CalcPBRLightingForSingleLightSource(
+            albedo, metallic, roughness,
+            normal, cameraPosition - position, uPointLights[i].pos - position,
+            pointLightColor
+        );
+    }
+
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0 / 2.2));
+
     return color;
 }
 )";
