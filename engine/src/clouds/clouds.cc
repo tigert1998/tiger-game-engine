32f82026db451401f6e9db4879a2ad04fb0f05c5
Xiaohu Tang <tigertang.zju@qq.com>
2023-05-18 10:48:20 +0800

    Add atmospheric scattering

--- engine/src/clouds/clouds.cc
+++ engine/src/clouds/clouds.cc
@@ -140,7 +140,7 @@ float Powder(float d) {
 
 bool RaySphereIntersection(
     vec3 rayOrigin, vec3 rayDirection,
-    vec3 sphereCenter, float radius, out vec3 position
+    vec3 sphereCenter, float radius, out float t, out vec3 position
 ) {
     float a = dot(rayDirection, rayDirection);
     float b = 2 * dot(rayDirection, rayOrigin - sphereCenter);
@@ -150,11 +150,13 @@ bool RaySphereIntersection(
     if (discr < 0) return false;
     float t0 = (-b - sqrt(discr)) / (2 * a);
     if (t0 > 0) {
+        t = t0;
         position = rayOrigin + t0 * rayDirection;
         return true;
     }
     float t1 = (-b + sqrt(discr)) / (2 * a);
     if (t1 > 0) {
+        t = t1;
         position = rayOrigin + t1 * rayDirection;
         return true;
     }
@@ -322,8 +324,124 @@ float ComputeFogAmount(vec3 startPosition, float factor) {
 }
 
 vec3 ComputeSkyColor(vec3 rayDirection) {
-    float t = 0.5 * (rayDirection.y + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    const float M_PI = 3.1415926;
+
+    const int viewSamples = 16;
+    const int lightSamples = 8;
+
+    const float I_sun = 20;                                   // Intensity of the sun
+    const vec3 beta_R = vec3(5.8e-5f, 13.5e-5f, 33.1e-5f);    // Rayleigh scattering coefficient
+    const float beta_M = 21e-5f;                              // Mie scattering coefficient
+    const float H_R = (uInnerHeight + uOuterHeight) * 0.1332; // Rayleigh scale height
+    const float H_M = (uInnerHeight + uOuterHeight) * 0.0200; // Mie scale height
+    const float g = 0.888;                                    // Mie scattering direction - 
+                                                              //  - anisotropy of the medium
+
+    // Normalize the light direction
+    vec3 sunDir = normalize(-GetSunLight().dir);
+
+    float t0, t1;
+    vec3 position;
+    bool hit = RaySphereIntersection(
+        uCameraPosition, rayDirection, 
+        uSphereCenter, uSphereRadius + uInnerHeight + uOuterHeight,
+        t0, position
+    );
+    if (!hit) return vec3(0);
+    hit = RaySphereIntersection(
+        uCameraPosition, rayDirection,
+        uSphereCenter, uSphereRadius,
+        t1, position
+    );
+    float t = hit ? min(t0, t1) : t0;
+
+    float segmentLen = t / float(viewSamples);
+
+    float tCurrent = 0.0f; 
+
+    // Rayleigh and Mie contribution
+    vec3 sum_R = vec3(0);
+    vec3 sum_M = vec3(0);
+
+    // Optical depth 
+    float optDepth_R = 0.0;
+    float optDepth_M = 0.0;
+
+    // Mu: the cosine angle between the sun and ray direction
+    float mu = dot(rayDirection, sunDir);
+    float mu_2 = mu * mu;
+
+    //--------------------------------
+    // Rayleigh and Mie Phase functions
+    float phase_R = 3.0 / (16.0 * M_PI) * (1.0 + mu_2);
+
+    float g_2 = g * g;
+    float phase_M = 3.0 / (8.0 * M_PI) * 
+                          ((1.0 - g_2) * (1.0 + mu_2)) / 
+                          ((2.0 + g_2) * pow(1.0 + g_2 - 2.0 * g * mu, 1.5));
+    // Sample along the view ray
+    for (int i = 0; i < viewSamples; ++i)
+    {
+        // Middle point of the sample position
+        vec3 vSample = uCameraPosition + rayDirection * (tCurrent + segmentLen * 0.5);
+
+        // Height of the sample above the planet
+        float height = length(vSample - uSphereCenter) - uSphereRadius;
+
+        // Optical depth for Rayleigh and Mie scattering for current sample
+        float h_R = exp(-height / H_R) * segmentLen;
+        float h_M = exp(-height / H_M) * segmentLen;
+        optDepth_R += h_R;
+        optDepth_M += h_M;
+
+        //--------------------------------
+        // Secondary - light ray
+        float t;
+        vec3 position;
+        RaySphereIntersection(
+            vSample, sunDir,
+            uSphereCenter, uSphereRadius + uInnerHeight + uOuterHeight,
+            t, position
+        );
+        float segmentLenLight = t / float(lightSamples);
+        float tCurrentLight = 0.0;
+
+        // Light optical depth 
+        float optDepthLight_R = 0.0;
+        float optDepthLight_M = 0.0;
+
+        // Sample along the light ray
+        for (int j = 0; j < lightSamples; ++j)
+        {
+            // Position of the light ray sample
+            vec3 lSample = vSample + sunDir * 
+                           (tCurrentLight + segmentLenLight * 0.5);
+            // Height of the light ray sample
+            float heightLight = length(lSample - uSphereCenter) - uSphereRadius;
+
+            // TODO check sample above the ground
+            
+            optDepthLight_R += exp(-heightLight / H_R) * segmentLenLight;
+            optDepthLight_M += exp(-heightLight / H_M) * segmentLenLight;
+
+            // Next light sample
+            tCurrentLight += segmentLenLight;
+        }
+        // TODO check sample above ground
+
+        // Attenuation of the light for both Rayleigh and Mie optical depth
+        //  Mie extenction coeff. = 1.1 of the Mie scattering coeff.
+        vec3 att = exp(-(beta_R * (optDepth_R + optDepthLight_R) + 
+                         beta_M * 1.1f * (optDepth_M + optDepthLight_M)));
+        // Accumulate the scattering 
+        sum_R += h_R * att;
+        sum_M += h_M * att;
+
+        // Next view sample
+        tCurrent += segmentLen;
+    }
+
+    return I_sun * (sum_R * beta_R * phase_R + sum_M * beta_M * phase_M);
 }
 
 vec4 PostProcess(vec4 color, vec4 background, vec3 rayDirection, float fogAmount) {
@@ -346,16 +464,17 @@ void main() {
     vec3 rayDirection = normalize(vec3(worldCoord) - uCameraPosition);
 
     vec3 startPosition, endPosition;
+    float notUsedT;
     RaySphereIntersection(
         uCameraPosition, rayDirection, 
         uSphereCenter, uSphereRadius + uInnerHeight, 
-        startPosition
+        notUsedT, startPosition
     );
 	RaySphereIntersection(
         uCameraPosition, rayDirection,
         uSphereCenter, uSphereRadius + uInnerHeight + uOuterHeight,
-        endPosition
-    );	
+        notUsedT, endPosition
+    );
 
     vec4 background = vec4(ComputeSkyColor(rayDirection), 1);
     float fogAmount = ComputeFogAmount(startPosition, 0.00006);
