50dfb7521598c07641db33ba8fd658bedd065050
tigertang <tigertang.zju@qq.com>
2024-02-18 16:34:04 +0800

    Allow shadow zone to overlap

--- shaders/shadow/shadow_sources.glsl
+++ shaders/shadow/shadow_sources.glsl
@@ -1,29 +1,15 @@
+#ifndef SHADOW_SHADOW_SOURCES_GLSL_
+#define SHADOW_SHADOW_SOURCES_GLSL_
+
 #include "shadow/directional_shadow.glsl"
 
-float CalcShadow(vec3 position) {
-    if (uNumDirectionalShadows == 0) {
-        return 0;
-    }
-    DirectionalShadow directionalShadow = directionalShadows[0];
-
-    // select cascade layer
-    vec4 viewSpacePosition = uViewMatrix * vec4(position, 1);
-    float depth = -viewSpacePosition.z;
-
-    int layer = NUM_CASCADES - 1;
-    for (int i = 0; i < NUM_CASCADES - 1; i++) {
-        if (depth < directionalShadow.cascadePlaneDistances[i]) {
-            layer = i;
-            break;
-        }
-    }
-
+float CalcShadowForSingleCascade(DirectionalShadow directionalShadow, uint layer, vec3 position) {
     vec4 homoPosition = directionalShadow.viewProjectionMatrices[layer] * vec4(position, 1.0);
 
     position = homoPosition.xyz / homoPosition.w;
     position = position * 0.5 + 0.5;
     float currentDepth = position.z;
-    currentDepth = max(min(currentDepth, 1), 0);
+    currentDepth = clamp(currentDepth, 0, 1);
     // If the fragment is outside the shadow frustum, we don't care about its depth.
     // Otherwise, the fragment's depth must be between [0, 1].
 
@@ -44,3 +30,31 @@ float CalcShadow(vec3 position) {
 
     return shadow;
 }
+
+float CalcShadow(vec3 position) {
+    if (uNumDirectionalShadows == 0) {
+        return 0;
+    }
+    DirectionalShadow directionalShadow = directionalShadows[0];
+
+    // select cascade layer
+    vec4 viewSpacePosition = uViewMatrix * vec4(position, 1);
+    float depth = -viewSpacePosition.z;
+
+    float num = 0;
+    float denom = 0;
+    for (int i = 0; i < NUM_CASCADES; i++) {
+        float near = directionalShadow.cascadePlaneDistances[i * 2];
+        float far = directionalShadow.cascadePlaneDistances[i * 2 + 1]; 
+        if (near <= depth && depth < far) {
+            float shadow = CalcShadowForSingleCascade(directionalShadow, i, position);
+            float ratio = (far - depth) / (far - near);
+            num += shadow * ratio;
+            denom += ratio;
+        }
+    }
+
+    return num / denom;
+}
+
+#endif
