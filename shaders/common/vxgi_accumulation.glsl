0f1d96ccd4a8347c223049cbb872b09fe57fef1d
tigertang <tigertang.zju@qq.com>
2024-04-10 11:00:06 +0800

    Update accumulation equation reference

--- shaders/common/vxgi_accumulation.glsl
+++ shaders/common/vxgi_accumulation.glsl
@@ -4,9 +4,14 @@
 vec4 Accumulate(vec4 value, vec4 newValue) {
     // this accumulation equation is different from that in the original NVIDIA paper
     // it comes from wicked engine's blog:
-    // https://wickedengine.net/2017/08/30/voxel-based-global-illumination/
+    // https://wickedengine.net/voxel-based-global-illumination/
     value.rgb += (1 - value.a) * newValue.rgb;
     value.a += (1 - value.a) * newValue.a;
+
+    // the accumulation equation from the original NVIDIA paper
+    // value.rgb = value.a * value.rgb + (1 - value.a) * newValue.a * newValue.rgb;
+    // value.a = value.a + (1 - value.a) * newValue.a;
+
     return value;
 }
 
